05a531394d1aca11049d2bf5d5a63deab2983550
tizah <davidzagi93@gmail.com>
2019-08-19 11:02:16 +0100

    Add project files.

---
+++ AspNet-with-Heroku-and-Docker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNet_with_Heroku_and_Docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
